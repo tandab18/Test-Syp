b78bd586994b43cbe3183a96ea98d298c48ad706
David Tantscher <tandab18@htl-kaindorf.at>
2022-06-15 15:01:58 +0200

    Sprint2

--- src/main/java/Tester.java
+++ src/main/java/Tester.java
@@ -3,5 +3,8 @@ public class Tester
     public static void main(String[] args) {
         System.out.println("TRIER");
         System.out.println("END SPRINT 1");
+
+        System.out.println("START Sprint 2");
+        System.out.println("END Sprint 2");
     }
 }
